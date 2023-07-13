507c4747632778dbc189065410f04f07e7719c3c
GuliMaster <dakurushin@gmail.com>
2023-07-13 14:43:01 +0300

    Added logic of Project
    Project is able to recognise and convert speech. Also according to test case it joins random numbers to each word.

---
+++ src/Components/Recognizer/Recognizer.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import s from './Recognizer.module.css';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+const Recognizer = () => {
+    interface Speech {
+        transcript: string,
+        listening: boolean,
+        resetTranscript: ()=>void,
+        browserSupportsSpeechRecognition: boolean
+    }
+
+    const speech:Speech = useSpeechRecognition();
+
+    if (!speech.browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
+
+    const startHandler = (event: React.MouseEvent<HTMLButtonElement>):void => {
+        SpeechRecognition.startListening();
+    }
+    const stopHandler = (event: React.MouseEvent<HTMLButtonElement>):void => {
+        SpeechRecognition.stopListening();
+    }
+    const resetHandler = (event: React.MouseEvent<HTMLButtonElement>):void => {
+        speech.resetTranscript();
+    }
+
+    const addNumberToWord = (text: string):string => {
+        return text?text.split(' ').map(word => word+Math.floor(Math.random()*100)).join(' '):'';
+    }
+
+    return (
+        <div className={s.container}>
+            <div className={s.recognizer}>
+                <p className={s.mic}>Микрофон: {speech.listening ? 'Включен' : 'Выключен'}</p>
+                <button className={s.button} onClick={startHandler}>Старт</button>
+                <button className={s.button} onClick={stopHandler}>Стоп</button>
+                <button className={s.button} onClick={resetHandler}>Обновить</button>
+                <p className={s.speech}>{addNumberToWord(speech.transcript)}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Recognizer;
